6ec2c4eade1b215f5469bff0bb5a4cf72571fef4
ve7fet@gmail.com <ve7fet@gmail.com>
2011-08-20 04:31:24 +0000

    Add changelog, remove mpd and one build targets and associated files.

--- branches/0.7.5/config/chroot_local-hooks/001-rmpkg.sh
+++ branches/0.7.5/config/chroot_local-hooks/001-rmpkg.sh
@@ -2,15 +2,6 @@
 
 echo "($0)"
 
-if [ $(dpkg-query --show | cut -f1 | grep "^asterisk$" | wc -l) -gt 0  ] ; then
-    echo "($0) Building Voyage ONE.  No remove package"
-    exit 0
-fi
-if [ $(dpkg-query --show | cut -f1 | grep "^mpd$" | wc -l) -gt 0  ] ; then
-    echo "($0) Building Voyage MPD.  No remove package"
-    exit 0
-fi
-   
 RMPKGS="
 libtext-charwidth-perl \
 libtext-iconv-perl \
